a7ca9c892539a1bfbb42fc5281da8af12cdfd41a
drewcolmenar <drewgcolmenar@gmail.com>
2019-03-30 13:47:32 -0700

    just test

--- app/src/main/java/com/example/drew1/MainActivity.java
+++ app/src/main/java/com/example/drew1/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.drew1;
-
+//drew test
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
